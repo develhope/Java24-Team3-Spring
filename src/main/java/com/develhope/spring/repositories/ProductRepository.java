a2edfd3e46331b599f4ff0b99edb6e20aedd1f23
Matteo Solinas <matteo.solinas91@gmail.com>
2024-05-29 00:51:00 +0200

    fix repositories
    aggiunta annotazione @Repository

--- src/main/java/com/develhope/spring/repositories/ProductRepository.java
+++ src/main/java/com/develhope/spring/repositories/ProductRepository.java
@@ -2,8 +2,10 @@ package com.develhope.spring.repositories;
 
 import com.develhope.spring.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, UUID> {
 }
--- src/main/java/com/develhope/spring/repositories/ProductTypeRepository.java
+++ src/main/java/com/develhope/spring/repositories/ProductTypeRepository.java
@@ -2,8 +2,10 @@ package com.develhope.spring.repositories;
 
 import com.develhope.spring.entities.ProductTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductTypeRepository extends JpaRepository<ProductTypeEntity, UUID> {
 }
