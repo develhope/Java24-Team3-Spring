b14246dc51898eade6595cd0a12dfad0900b3811
Matteo Solinas <matteo.solinas91@gmail.com>
2024-05-29 00:50:27 +0200

    fix exceptions
    aggiunto il responseStatus Not Found

--- src/main/java/com/develhope/spring/exceptions/ProductNotFoundException.java
+++ src/main/java/com/develhope/spring/exceptions/ProductNotFoundException.java
@@ -1,6 +1,11 @@
 package com.develhope.spring.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException {
+
     public ProductNotFoundException() {
         super();
     }
