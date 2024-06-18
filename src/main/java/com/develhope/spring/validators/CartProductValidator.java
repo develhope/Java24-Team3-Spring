f454f9d96ccf4969f14b5dcde2187f58b70c7e03
Matteo Solinas <matteo.solinas91@gmail.com>
2024-06-18 14:44:17 +0200

    fix cartProductValidator
    corretto il validator, ora non è possibile inserire un prodotto con quantity <= 0

--- src/main/java/com/develhope/spring/validators/CartProductValidator.java
+++ src/main/java/com/develhope/spring/validators/CartProductValidator.java
@@ -10,7 +10,7 @@ public class CartProductValidator {
     public void validateCartProduct(CartProductDto cartProductDto) throws InvalidCartProductException {
         if (cartProductDto.getCartId() == null
                 && cartProductDto.getProductId() == null
-                && cartProductDto.getQuantity() < 0) {
+                && cartProductDto.getQuantity() <= 0) {
             throw new InvalidCartProductException("Cart product requires a valid cart, product, and a quantity > 0.");
         }
     }
